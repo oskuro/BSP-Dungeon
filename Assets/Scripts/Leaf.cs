4b213fdbe688ced52734485f4cc48841e0dbed1c
Lukas <lukas.isaksson@gmail.com>
2024-04-25 15:21:53 +0200

    BSP BABY

--- Assets/Scripts/Leaf.cs
+++ Assets/Scripts/Leaf.cs
@@ -5,11 +5,13 @@ using System.Drawing;
 using System.Numerics;
 using System;
 using UnityEngine;
-using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class Leaf 
 {
     const int MIN_LEAF_SIZE = 6;
+    float splitCompare = 1.25f;
+    float horizSplitChance = 0.5f;
     public int y, x, width, height;
     public Leaf leftChild;
     public Leaf rightChild;
@@ -33,12 +35,11 @@ public class Leaf
         // if the width is >25% larger than height, we split vertically		
         // if the height is >25% larger than the width, we split horizontally		
         // otherwise we split randomly		
-        Random r = new Random();
-        var splitH = r.NextDouble() > 0.5;
+        var splitH = Random.Range(0f, 1f) > horizSplitChance;
         
-        if (width > height && width / height >= 1.25)
+        if (width > height && width / height >= splitCompare)
             splitH = false;
-        else if (height > width && height / width >= 1.25)
+        else if (height > width && height / width >= splitCompare)
             splitH = true;
 
         var max = (splitH ? height : width) - MIN_LEAF_SIZE; // determine the maximum height or width
@@ -47,7 +48,7 @@ public class Leaf
             return false; // the area is too small to split any more...		
 
         // determine where we're going to split		
-        var split = r.Next(MIN_LEAF_SIZE, max);
+        var split = Random.Range(MIN_LEAF_SIZE, max);
 
         // create our left and right children based on the direction of the split		
         if (splitH)
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,51 +1,81 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
-using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
-    const int MAX_LEAF_SIZE = 20;
-    Stack<Leaf> leafs = new Stack<Leaf>();
+    [SerializeField] int maxLeafSize = 100;
+    [SerializeField] float InvertedSplitChance = 0.25f;
+    [SerializeField] GameObject plane = null;
 
+    Color[] colors = { Color.white };
+    Stack<Leaf> leaves = new Stack<Leaf>();
     Leaf leaf;
+
+    Texture2D tex;
+
     private void Start()
     {
+        SetTexture();
+        SplitLeaves();
+    }
 
-        Texture2D sprMap = new Texture2D(500, 500);
+    private void SetTexture()
+    {
+        plane.GetComponent<Renderer>().material.mainTexture = tex;
 
-        Random r = new Random();
-        Leaf root = new Leaf(0, 0, sprMap.width, sprMap.height);
-        leafs.Push(root);
+        
+
+        tex = new Texture2D(500, 500);
+        for (int x = 10; x > 110; x++)
+        {
+            for (int y = 10; y > 110; y++)
+            {
+                tex.SetPixel(x, y, Color.green);
+            }
+        }
+        tex.Apply();
+
+      
+        
+    }
+
+    private void SplitLeaves()
+    {
+        Leaf root = new Leaf(0, 0, tex.width, tex.height);
+        leaves.Push(root);
 
         bool did_split = true;
 
         while (did_split)
         {
             did_split = false;
-            foreach (Leaf l in leafs)
+
+            // We can't modify the list we are iterating over so we make a copy.
+            List<Leaf> tempLeaves = new List<Leaf>(leaves);
+            foreach (Leaf l in tempLeaves)
             {
                 if (l.leftChild == null && l.rightChild == null)
                 {
                     // if this Leaf is not already split...		
                     // if this Leaf is too big, or 75% chance...			
-                    if (l.width > MAX_LEAF_SIZE || l.height > MAX_LEAF_SIZE || r.NextDouble() > 0.25)
+                    if (l.width > maxLeafSize || l.height > maxLeafSize || Random.Range(0f,1f) > InvertedSplitChance)
                     {
                         if (l.Split())
                         // split the Leaf!				
                         {
-                            // if we did split, push the child leafs to the Vector so we can loop into them next					
-                            leafs.Push(l.leftChild);
-                            leafs.Push(l.rightChild);
+                            //Debug.Log("x: " + l.leftChild.x + " y: " + l.leftChild.y + " w: " + l.leftChild.width + " h: " + l.leftChild.height);
+                            
+                            //tex.SetPixels(l.leftChild.x, l.leftChild.y, l.leftChild.width -1, l.leftChild.height -1, colors);
+                            // if we did split, push the child leafs to the stack so we can loop into them next					
+                            leaves.Push(l.leftChild);
+                            leaves.Push(l.rightChild);
                             did_split = true;
-                            Debug.Log("Did Split: " + l.leftChild + ", " + l.rightChild);
                         }
                     }
                 }
             }
         }
     }
+ }
 
-
-}
